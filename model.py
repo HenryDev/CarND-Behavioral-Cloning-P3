422d29cf32dad5a2c68bf61aa0801cd535c99a13
HenryDev <HenryDev@users.noreply.github.com>
2017-03-14 21:21:04 -0500

    lower epoch

--- model.py
+++ model.py
@@ -63,7 +63,7 @@ def make_model():
 model = make_model()
 model.compile('adam', 'mse')
 x, y = read_data()
-model.fit(x, y, nb_epoch=3, validation_split=0.2)
+model.fit(x, y, nb_epoch=1, validation_split=0.2)
 # model.fit_generator(training_generator,len(),1)
 # print(history.history.keys())
 

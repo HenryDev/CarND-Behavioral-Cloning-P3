78df3fefd42bc3864a94f6a66506738ddbe003c9
HenryDev <HenryDev@users.noreply.github.com>
2017-03-21 16:29:54 -0500

    working model

--- model.py
+++ model.py
@@ -61,5 +61,5 @@ def make_model():
 model = make_model()
 model.compile('adam', 'mse')
 x, y = read_data()
-model.fit(x, y, validation_split=0.2)
+model.fit(x, y, nb_epoch=3, validation_split=0.2)
 model.save('model.h5')
